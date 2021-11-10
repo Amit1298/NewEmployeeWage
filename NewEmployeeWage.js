3c7aba7aac4b52897864b80fa838a368145f8921
Amit <amitkr24031995@gmail.com>
2021-11-10 10:57:25 +0530

    Adding Attendance

---
+++ NewEmployeeWage.js
@@ -0,0 +1,13 @@
+//UC1
+
+{const Is_Absent = 0
+    let empcheck = Math.floor(Math.random()*10)%2;
+    if(empcheck == Is_Absent)
+    {
+        console.log("UC1 Employee is Absent");
+    }
+    else
+    {
+        console.log("Employee is Present");
+    }
+    }
