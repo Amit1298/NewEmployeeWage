7bab8fe05ad18b30d2d03b72c7a3d6de2f38ea70
Amit <amitkr24031995@gmail.com>
2021-11-11 09:22:36 +0530

    Adding while EmpWage

--- NewEmployeeWage.js
+++ NewEmployeeWage.js
@@ -58,12 +58,40 @@
 
 
 // UC4
+// const IS_PART_TIME = 1;
+// const IS_FULL_TIME = 2;
+// const PART_TIME_HOURS = 4;
+// const FULL_TIME_HOURS = 8;
+// const WAGE_PER_HOURS = 20;
+// const NUM_OF_WORKING_DAYS = 20;
+// function getWorkingHours(empCheck){
+//     switch (empCheck){
+//         case IS_PART_TIME:
+//             return PART_TIME_HOURS;
+//         case IS_FULL_TIME:
+//             return FULL_TIME_HOURS;
+//         default:
+//             return 0;  
+//     }
+// }
+
+// let totalEmpHrs = 0;
+// for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+//     let empCheck = Math.floor(Math.random() *10)%3;
+//     totalEmpHrs += getWorkingHours(empCheck);
+// }
+// let empWage = totalEmpHrs * WAGE_PER_HOURS;
+// console.log("Hours "+totalEmpHrs +" Emp Wage: "+ empWage);
+
+
+// UC5
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH =160;
 function getWorkingHours(empCheck){
     switch (empCheck){
         case IS_PART_TIME:
@@ -76,9 +104,11 @@ function getWorkingHours(empCheck){
 }
 
 let totalEmpHrs = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
-    let empCheck = Math.floor(Math.random() *10)%3;
+let totalWorkingDays = 0;
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++ ;
+    let empCheck = Math.floor(Math.random()*10)%3;
     totalEmpHrs += getWorkingHours(empCheck);
 }
 let empWage = totalEmpHrs * WAGE_PER_HOURS;
-console.log("Hours "+totalEmpHrs +" Emp Wage: "+ empWage);
+console.log("Total Days: "+totalWorkingDays+" Hours "+totalEmpHrs +" Emp Wage: "+ empWage);
