f02279c6c72ef96a559935264569e8da56e30af4
Amit <amitkr24031995@gmail.com>
2021-11-11 10:05:13 +0530

    Adding Map Emp Wage

--- NewEmployeeWage.js
+++ NewEmployeeWage.js
@@ -115,6 +115,43 @@
 
 
 //UC6
+// const IS_PART_TIME = 1;
+// const IS_FULL_TIME = 2;
+// const PART_TIME_HOURS = 4;
+// const FULL_TIME_HOURS = 8;
+// const WAGE_PER_HOURS = 20;
+// const NUM_OF_WORKING_DAYS = 20;
+// const MAX_HRS_IN_MONTH =160;
+// function getWorkingHours(empCheck){
+//     switch (empCheck){
+//         case IS_PART_TIME:
+//             return PART_TIME_HOURS;
+//         case IS_FULL_TIME:
+//             return FULL_TIME_HOURS;
+//         default:
+//             return 0;  
+//     }
+// }
+
+// function calcDailyWage(empHrs){
+//     return empHrs * WAGE_PER_HOURS;
+// }
+
+// let totalEmpHrs = 0;
+// let totalWorkingDays = 0;
+// let empDailyWageArr = new Array();
+// while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+//     totalWorkingDays++ ;
+//     let empCheck = Math.floor(Math.random()*10)%3;
+//     let empHrs = getWorkingHours(empCheck);
+//     totalEmpHrs += empHrs;
+//     empDailyWageArr.push(calcDailyWage(empHrs));
+// }
+// let empWage = calcDailyWage(totalEmpHrs);
+// console.log("Total Days: "+totalWorkingDays+" Hours "+totalEmpHrs +" Emp Wage: "+ empWage);
+
+
+// UC8
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -140,12 +177,17 @@ function calcDailyWage(empHrs){
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
     totalWorkingDays++ ;
     let empCheck = Math.floor(Math.random()*10)%3;
     let empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
     empDailyWageArr.push(calcDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
 }
-let empWage = calcDailyWage(totalEmpHrs);
-console.log("Total Days: "+totalWorkingDays+" Hours "+totalEmpHrs +" Emp Wage: "+ empWage);
+console.log(empDailyWageMap);
+function totalWages(totalWage, dailyWage){
+    return totalWage + dailyWage;
+}
+console.log("Total Days: "+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
